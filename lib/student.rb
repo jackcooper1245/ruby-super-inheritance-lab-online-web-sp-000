9dc6ef0995431c9a00f1eac959e171b7b32bfbd0
Jack Cooper <jackcooper1245@gmail.com>
2020-04-22 10:19:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,11 @@
 class Student
 
 def hello
+  puts "Hey there! I'm so excited to learn stuff."
+end
 
-puts "Hey there! I'm so excited to learn stuff."
-
+def raise_hand 
+  puts "Pick me!"
 end
 
 end
